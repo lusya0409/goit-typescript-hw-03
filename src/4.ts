34826589661b0c85949d702bd55d173a88f4a396
lusya0409 <lusya0409@gmail.com>
2023-11-10 19:10:02 +0200

    vhenges hw3 ts1-4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  public getSignature(this: Key) {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  public getKey(this: Person) {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  public comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+    this.door = false;
+    this.tenants = [];
+  }
+  public openDoor(key: Key) {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
